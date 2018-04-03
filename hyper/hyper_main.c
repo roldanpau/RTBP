42324f40a2c2efb4a8d5c25c4cd069b267e5f33f
roldanpau <roldan.pau@gmail.com>
2018-04-03 11:46:31 -0400

    Fixed compiler warnings

--- hyper/hyper_main.c
+++ hyper/hyper_main.c
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>	// EXIT_SUCCESS, EXIT_FAILURE
+#include <string.h>	// strcmp
 #include <gsl/gsl_errno.h>	// gsl_set_error_handler_off
 #include <prtbp.h>	// section_t
 #include "hyper.h"	// hyper
