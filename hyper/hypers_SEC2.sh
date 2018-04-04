077e0e403d67e01dbf6c4900400cb1cfceca8d08
roldanpau <roldan.pau@gmail.com>
2018-04-04 12:19:19 -0400

    Moved sec1sec2 to sec1sec2_2d

--- hyper/hypers_SEC2.sh
+++ hyper/hypers_SEC2.sh
@@ -1,5 +1,5 @@
 echo "0.95387536e-3 SEC2 2" >hypers.dat
-cut -d ' ' -f 1-3 ../sec1sec2/sec1sec2.res >>hypers.dat
+cut -d ' ' -f 1-3 ../sec1sec2/sec1sec2_2d.res >>hypers.dat
 hypers <hypers.dat >hypers.res
 
 rm hypers.dat 
