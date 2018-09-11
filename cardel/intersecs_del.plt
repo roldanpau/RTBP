0e0d238973564e84de7e0c163c28ba348cd7e490
Pau Roldan <roldan.pau@gmail.com>
2018-09-11 11:11:55 -0400

    Added intersecs_del.plt

---
+++ cardel/intersecs_del.plt
@@ -0,0 +1,5 @@
+#plot [:-1.49] \
+
+plot \
+"intersecs_unst_br1_del.res" u 1:3, \
+"intersecs_st_br1_del.res" u 1:3
---
+++ cardel/intersecs_st_br1_del.sh
@@ -0,0 +1,10 @@
+FILEROOT=intersecs_st_br1_del
+DATFILE=$FILEROOT.dat
+TMPFILE=$FILEROOT.tmp
+RESFILE=$FILEROOT.res
+
+echo "0.95387536e-3" >$DATFILE
+cut -d ' ' -f 1,5 ../intersec/intersecs_st_br1.res >$TMPFILE
+awk '{print $1, $2, 0}' $TMPFILE >>$DATFILE
+cardels_2d <$DATFILE >$RESFILE
+rm $TMPFILE $DATFILE
