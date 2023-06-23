2562617cf5b080619728a541b68b88e807afc65d
Pau Roldan <roldan.pau@gmail.com>
2023-06-23 12:02:30 +0200

    Forgot to compute absolute value for error in omega_neg_test. Fixed.

--- outer_circ/omega_neg.sh
+++ outer_circ/omega_neg.sh
@@ -10,7 +10,7 @@ cut -d ' ' -f 1 omega_pos_st_br1.res > temp4
 cut -d ' ' -f 1 omega_pos_st_br2.res > temp5
 
 # Select only the first 117 lines, corresponding to energies H<-1.4854.
-# The reason for this is because, for larger energies, the p.o. is not
-# in almost resonance anymore, and g=0/PI is not a surface of section.
+# The reason for this is because, for larger energies, g=0/PI is not a surface
+# of section for the flow (either for po or hom.).
 paste -d ' ' temp1 temp2 temp3 temp4 temp5 | sed -n '1,117p' > $resfile
 #rm temp1 temp2 temp3 temp4 temp5
