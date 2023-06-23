60f9b25c28a8d148d25aaf22099646a82343ab71
Pau Roldan <roldan.pau@gmail.com>
2023-06-23 12:03:45 +0200

    Recomputed B_in.

--- inner_ell_stoch/inner_ell_stoch.sh
+++ inner_ell_stoch/inner_ell_stoch.sh
@@ -1,16 +1,17 @@
 # Compute integral A_ín(I) related to outer map of elliptic problem.
 
-lines=`wc -l ../outer_circ/omega_neg_SECg_br1.res | cut -d ' ' -f 1`
-
-echo $lines
+#lines=`wc -l ../outer_circ/omega_neg_SECg_br1.res | cut -d ' ' -f 1`
+lines=117
 
 echo "0.95387536e-3" >inner_ell_stoch.dat	# mu
 
-# Select only the first $lines lines, corresponding to energy levels 
-# H <= -1.4894
+# Select only the first 117 lines, corresponding to energies H<-1.4854.
+# The reason for this is because, for larger energies, g=0/PI is not a surface
+# of section for the flow (either for po or hom.).
+
 cut -d ' ' -f 1 ../portbp/porbits.res | head -n $lines > temp1 	# H
-cut -d ' ' -f 1-4 ../prtbp_del_car/prtbp_del_cars_SECg.res | head -n $lines \
-    > temp2  # periodic point p
+cut -d ' ' -f 1-4 ../cardel/periodicorbits_del.res | head -n $lines \
+	> temp2  # periodic point p (in Delaunay coords).
 
 paste -d ' ' temp1 temp2 >> inner_ell_stoch.dat
 rm temp1 temp2 
