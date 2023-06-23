2aa724849e5058d1f4711601b9b906cce4a9a0ed
Pau Roldan <roldan.pau@gmail.com>
2023-06-23 19:47:56 +0200

    Compute only first 117 lines of omega_neg/omega_pos

--- outer_circ/omega_pos_st_br2.sh
+++ outer_circ/omega_pos_st_br2.sh
@@ -17,10 +17,10 @@ cut -d ' ' -f 2-5 ../intersec/intersecs_st_br2.res > temp2
 # t: integration time to reach homoclinic point z from z_u
 cut -d ' ' -f 6 ../intersec/intersecs_st_br2.res > temp3
 
-# Select only the first 136 lines, corresponding to energies H<=-1.4494.
+# Select only the first 117 lines, corresponding to energies H<=-1.4494.
 # The reason for this is because, for larger energies, the p.o. is not 
 # in almost resonance anymore, and g=0/PI is not a surface of section.
-paste -d ' ' temp1 temp2 temp3 | sed -n '1,136p' >>$datfile
+paste -d ' ' temp1 temp2 temp3 | sed -n '1,117p' >>$datfile
 rm temp1 temp2 temp3
 
 ./outer_circ_stoch < $datfile > $resfile 2> $errfile
